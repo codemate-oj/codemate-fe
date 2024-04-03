23866467fff4bd64c6822972bbc42867871bd808
洋 <94534613+BoyYangzai@users.noreply.github.com>
2024-04-04 02:55:14 +0800

    chore: update dep & setting

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,6 @@
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import React from "react";
 import Image from "next/image";
-
+import { Carousel } from "antd";
 const HomeLayout = ({
   children,
 }: Readonly<{
@@ -9,20 +8,19 @@ const HomeLayout = ({
 }>) => {
   return (
     <>
-      <Carousel className="w-full max-w-full" opts={{ loop: true }}>
-        <CarouselContent>
-          <CarouselItem>
-            <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
-          </CarouselItem>
-          <CarouselItem>
-            <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
-          </CarouselItem>
-          <CarouselItem>
-            <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+      <Carousel autoplay>
+        <div>
+          <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
+        </div>
+        <div>
+          <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
+        </div>
+        <div>
+          <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
+        </div>
+        <div>
+          <Image src="/img/carousel/1.jpg" alt="carousel-1" width={1920} height={320} />
+        </div>
       </Carousel>
       {children}
     </>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Image from "next/image";
-import Navigation, { NavItemType } from "@/components/home/navigation";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 
-import "./globals.css";
 import UserLogin from "@/components/home/user-login";
 import { cn } from "@/lib/utils";
 import PageFooter from "@/components/home/page-footer";
+import Navigation, { NavItemType } from "@/components/home/navigation";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,11 +19,11 @@ export const metadata: Metadata = {
 
 const mainRoutes: NavItemType[] = [
   {
-    name: "告示墙",
+    name: "告示墙(公告)",
     path: "/bulletin",
   },
   {
-    name: "修炼场",
+    name: "修炼场(题库)",
     path: "/",
   },
   {
@@ -45,16 +47,18 @@ export default function RootLayout({
   return (
     <html lang="zh-cn">
       <body className={cn(inter.className)}>
-        <header className="flex items-center justify-between m-auto py-2 max-w-screen-xl px-10 xl:px-0">
-          <section className="flex items-center gap-x-2.5">
-            <Image src="/img/logo.png" alt="website-logo" width={80} height={80} />
-            <h1 className="text-2xl font-bold">AI推题，高效有趣玩OJ</h1>
-          </section>
-          <Navigation routes={mainRoutes} />
-          <UserLogin />
-        </header>
-        <main className="max-w-full">{children}</main>
-        <PageFooter />
+        <AntdRegistry>
+          <header className="flex items-center justify-between m-auto py-2 max-w-screen-xl px-10 xl:px-0">
+            <section className="flex items-center gap-x-2.5">
+              <Image src="/img/logo.png" alt="website-logo" width={80} height={80} />
+              <h1 className="text-2xl font-bold">AI推题，高效有趣玩OJ</h1>
+            </section>
+            <Navigation routes={mainRoutes} />
+            <UserLogin />
+          </header>
+          <main className="max-w-full">{children}</main>
+          <PageFooter />
+        </AntdRegistry>
       </body>
     </html>
   );
