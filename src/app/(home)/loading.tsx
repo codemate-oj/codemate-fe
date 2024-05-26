6eef0b3a3efb12c1d379d80d62e0552e3dc2f867
Kirito <kiritoclzh@gmail.com>
2024-05-27 03:24:57 +0800

    feat: 添加suspense

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,12 @@
+import Skeleton from "@/components/ui/skeleton";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="p-5">
+      <Skeleton />
+    </div>
+  );
+};
+
+export default Loading;
