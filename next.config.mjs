26e3c320a91aceb0a8bb01e412ed257a5d592a2f
ChlorineC <kiritoclzh@gmail.com>
2024-06-15 18:27:02 +0800

    Fix/clzh/next config (#34)
    * fix: next config
    
    * fix: ts error and type check for gh action
    
    * fix: ts error
    
    * chore: disable typedRoutes for nextjs
    
    * chore: change hostname
    
    * fix: url prefix

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       return [
         {
           source: "/api/:path*",
-          destination: `${process.env.API_URL ?? "https://www.aioj.net/"}:path*`,
+          destination: `${process.env.API_URL ?? "https://beta.aioj.net/api/"}:path*`,
         },
       ];
     }
