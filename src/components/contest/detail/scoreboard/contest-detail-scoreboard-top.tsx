5396a6cdb2ad6a340de639b3aaff2e4db4bf0ed2
雷闳焜 <12021546+lei-hongkun@user.noreply.gitee.com>
2024-06-18 14:28:04 +0800

    feat: 标题布局换行

--- src/components/contest/detail/scoreboard/contest-detail-scoreboard-top.tsx
+++ src/components/contest/detail/scoreboard/contest-detail-scoreboard-top.tsx
@@ -10,16 +10,12 @@ interface ScoreboardTopProps {
 const ScoreboardTop: React.FC<ScoreboardTopProps> = (props) => {
   const { title, attend, beginAt } = props;
   return (
-    <div className="mb-10">
-      <div className="header flex justify-between">
-        <div className="flex">
-          <div className="title text-2xl font-bold">比赛:{title}</div>
-          <div className="text-2xl font-bold text-[#FF7D37]">&nbsp;&nbsp;参赛人数：{attend}人</div>
-        </div>
-        <div>
-          <span className="min-w-40 bg-[rgb(249,249,249)] px-2 py-1 text-center text-sm text-[rgb(121,121,121)]">
-            比赛时间：{formatTime(beginAt)}
-          </span>
+    <div className="mb-6">
+      <div className="header flex flex-wrap justify-between">
+        <div className="title mb-4 text-2xl font-bold">比赛:{title}</div>
+        <div className="mb-4 text-2xl font-bold text-[#FF7D37]">&nbsp;&nbsp;参赛人数：{attend}人</div>
+        <div className="mb-4 min-w-40 bg-[rgb(249,249,249)] px-2 py-1 text-center text-sm text-[rgb(121,121,121)]">
+          比赛时间：{formatTime(beginAt)}
         </div>
       </div>
     </div>
