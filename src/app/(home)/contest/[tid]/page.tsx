f0e3cf89cfb072aa069a875ef375e84dd801c321
Lhk0001 <98312294+Lhk0001@users.noreply.github.com>
2024-06-25 03:28:47 +0800

    feat: 竞技场比赛题单页面 (#29)
    * feat: 比赛题单，提交记录和评测记录，状态枚举值待修改
    
    * feat: 状态枚举组件修改
    
    * feat: 变量规范修改，judge-status type提取
    
    * feat: judge-status常量强类型设置
    
    * feat: 题单逻辑修改，button color 改为primary，meatdata添加，报名状态逻辑修改
    
    * feat: 题单逻辑修改，button color 改为primary，meatdata添加，报名状态逻辑修改
    
    * feat(clzh): 登录注册系统 & 个人中心部分页面 (#32)
    
    * feat: user/record
    
    * feat: 错题列表
    
    * feat: 错题和收藏列表
    wip: graphql api接入
    
    * chore: 提取Tag组件
    
    * feat: wip gql
    
    * revert: 回退graphql更新
    
    * fix: 无感登录态刷新在测试服下的表现
    
    * fix: remoteUrl()空兼容
    
    * fix: hook change
    
    * feat: 侧边栏默认全部展开
    
    * chore: update schema
    
    * chore: make sidepad bulletin better
    
    * chore: 修改get请求缓存策略
    
    * chore: 增加tailwind prettier插件
    
    * feat: 增加captcha功能
    
    * feat: 短信验证码后台校验
    
    * feat: 完成用户注册modal
    
    * fix: modal工作问题
    
    * chore: 优化细节
    
    * feat: 打通注册流程
    
    * feat: error handling
    
    * chore: clear codes
    
    * chore: 整理要发布的代码
    
    * format with prettier
    
    * fix: next config (#33)
    
    * fix: next config
    
    * fix: ts error and type check for gh action
    
    * fix: ts error
    
    * chore: disable typedRoutes for nextjs
    
    * Fix/clzh/next config (#34)
    
    * fix: next config
    
    * fix: ts error and type check for gh action
    
    * fix: ts error
    
    * chore: disable typedRoutes for nextjs
    
    * chore: change hostname
    
    * fix: url prefix
    
    * fix: 报名状态加比赛状态题单显示问题修正
    
    * feat: added bulletin list page (#31)
    
    * feat: added bulletin list page
    
    * feat: detail page demo
    
    * chore: make vercel happy
    
    ---------
    
    Co-authored-by: KiritoKing <kiritoclzh@gmail.com>
    
    * feat: 比赛题单，提交记录和评测记录，状态枚举值待修改
    
    * feat: 状态枚举组件修改
    
    * feat: 变量规范修改，judge-status type提取
    
    * feat: judge-status常量强类型设置
    
    * feat: 题单逻辑修改，button color 改为primary，meatdata添加，报名状态逻辑修改
    
    * feat: 题单逻辑修改，button color 改为primary，meatdata添加，报名状态逻辑修改
    
    * fix: 报名状态加比赛状态题单显示问题修正
    
    * feat: mearg/main主分支更新
    
    * feat: eslint后端类型未添加
    
    * feat: pnpm-lock文件解决
    
    * feat: 依赖修改
    
    ---------
    
    Co-authored-by: 雷闳焜 <12021546+lei-hongkun@user.noreply.gitee.com>
    Co-authored-by: ChlorineC <kiritoclzh@gmail.com>
    Co-authored-by: Xi Sun <sunxi.amiya@gmail.com>

--- src/app/(home)/contest/[tid]/page.tsx
+++ src/app/(home)/contest/[tid]/page.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ContestDetail from "@/components/contest/detail/detail";
 import PageTitle from "@/components/common/page-title";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "竞技场-比赛详情 - CODEMATE",
+};
 const Detail = ({ params }: { params: { tid: string } }) => {
   const { tid } = params;
   return (
