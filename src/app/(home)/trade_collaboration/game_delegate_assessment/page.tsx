22ed8d095d8fb556c20d062ae74d9f1c31801cc5
xiaokeqiang <xiaokeqiang@meituan.com>
2024-06-25 14:06:18 +0800

    feat: 创建页面

---
+++ src/app/(home)/trade_collaboration/game_delegate_assessment/page.tsx
@@ -0,0 +1,17 @@
+import PageTitle from "@/components/common/page-title";
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "商务合作 - CODEMATE",
+};
+
+const GameDelegateAssessment = () => {
+  return (
+    <div>
+      <PageTitle>赛事委托评测</PageTitle>
+    </div>
+  );
+};
+
+export default GameDelegateAssessment;
---
+++ src/app/(home)/trade_collaboration/training_org_assessment/page.tsx
@@ -0,0 +1,17 @@
+import PageTitle from "@/components/common/page-title";
+import type { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "商务合作 - CODEMATE",
+};
+
+const TrainingOrgAssessment = () => {
+  return (
+    <div>
+      <PageTitle>教培机构评测</PageTitle>
+    </div>
+  );
+};
+
+export default TrainingOrgAssessment;
