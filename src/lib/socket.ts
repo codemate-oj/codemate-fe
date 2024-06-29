e17458aaac5c214fc803ef0552e7c7f987875e9b
KiritoKing <kiritoclzh@gmail.com>
2024-06-30 00:44:36 +0800

    fix: ssr error

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,9 +1,6 @@
 export function getWsUrl(url: string, params: Record<string, string> = {}) {
   const _url = new URL(`/api${url}`, `wss://beta.aioj.net`);
   const searchParams = new URLSearchParams(params);
-  if (_url.host !== window.location.host && document.cookie.includes("sid=")) {
-    searchParams.append("sid", document.cookie.split("sid=")[1].split(";")[0]);
-  }
   _url.search = searchParams.toString();
   return _url.toString();
 }
