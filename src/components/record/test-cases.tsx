2180149d773b7d96b2020c149037ecd3f60b67ba
KiritoKing <kiritoclzh@gmail.com>
2024-06-29 23:59:36 +0800

    feat: 打通评测websocket

--- src/components/record/test-cases.tsx
+++ src/components/record/test-cases.tsx
@@ -20,6 +20,7 @@ export interface TestCaseType {
 }
 
 interface IProps {
+  rid: string;
   testCases: TestCaseType[];
 }
 
--- src/hooks/useRecordMainConn.ts
+++ src/hooks/useRecordMainConn.ts
@@ -1,49 +1,29 @@
 import { RecordFilterFormType } from "@/components/record/record-filter-form";
 import { PROGRAMMING_LANGS } from "@/constants/misc";
-import { useWebSocket } from "ahooks";
 import _ from "lodash";
-import { useRef, useState } from "react";
+import { useState } from "react";
+import { useHydroWebSocket } from "./useHydroWebSocket";
+import type { RecordRowType } from "@/components/record/record-list";
+import { getTimeFromObjectId } from "@/lib/utils";
 
 export default function useRecordMainConn(params: RecordFilterFormType = {}) {
-  const searchParams = new URLSearchParams(_.omitBy(params, _.isUndefined) as Record<string, string>);
-  const pingpongRef = useRef<NodeJS.Timeout | null>(null);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [latestRowChange, setLatestRowChange] = useState<any>();
-  const {} = useWebSocket(`ws://api.aioj.net/record-conn?${searchParams.toString()}`, {
-    reconnectLimit: 100,
-    reconnectInterval: 10000,
-    onOpen: (_, ws) => {
-      pingpongRef.current = setInterval(() => {
-        ws.send("ping");
-      }, 30000);
-    },
-    onMessage: async (message, ws) => {
-      const ObjectId = (await import("bson")).ObjectId;
-      if (message.data === "pong") return;
-      if (message.data === "ping") {
-        ws.send("pong");
-        return;
-      } else {
-        const data = JSON.parse(message.data).data;
-        setLatestRowChange({
-          rid: data.rdoc._id,
-          status: data.rdoc.status,
-          score: data.rdoc.score,
-          problem: data.pdoc,
-          submitBy: data.udoc,
-          time: data.rdoc.time,
-          memory: data.rdoc.memory,
-          lang: data.rdoc.lang === "_" ? "客观题" : PROGRAMMING_LANGS[data.rdoc.lang as keyof typeof PROGRAMMING_LANGS],
-          submitAt: ObjectId.createFromHexString(data.rdoc._id).getTimestamp(),
-        });
-      }
-    },
-    onClose: () => {
-      if (pingpongRef.current) {
-        clearInterval(pingpongRef.current);
-      }
+  const [latestRowChange, setLatestRowChange] = useState<RecordRowType>();
+  useHydroWebSocket("/record-conn", _.omitBy(params, _.isUndefined), {
+    onMessage: async (message) => {
+      const data = JSON.parse(message.data).data;
+      setLatestRowChange({
+        rid: data.rdoc._id,
+        status: data.rdoc.status,
+        score: data.rdoc.score,
+        problem: data.pdoc,
+        submitBy: data.udoc,
+        time: data.rdoc.time,
+        memory: data.rdoc.memory,
+        lang: data.rdoc.lang === "_" ? "客观题" : PROGRAMMING_LANGS[data.rdoc.lang as keyof typeof PROGRAMMING_LANGS],
+        submitAt: await getTimeFromObjectId(data.rdoc._id),
+      });
     },
   });
-
   return latestRowChange;
 }
