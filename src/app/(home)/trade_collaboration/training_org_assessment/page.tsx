91a9a5ac5205343bdf5f9a27dd54b5cc90ca884b
KiritoKing <kiritoclzh@gmail.com>
2024-07-03 00:45:22 +0800

    chore: clean clode

--- src/app/(home)/trade_collaboration/training_org_assessment/page.tsx
+++ src/app/(home)/trade_collaboration/training_org_assessment/page.tsx
@@ -4,13 +4,8 @@
  * @Description 教培机构页
  * */
 import PageTitle from "@/components/common/page-title";
-import type { Metadata } from "next";
 import Image from "next/image";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "商务合作 - CODEMATE",
-};
 
 const TrainingOrgAssessment = () => {
   return (
