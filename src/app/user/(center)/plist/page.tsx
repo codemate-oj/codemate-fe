5a531781b781fe765e9d4f9c6955fcb333f5349f
xiaokeqiang <xiaokeqiang@meituan.com>
2024-07-22 15:48:37 +0800

    feat: 初步创建样式

--- src/app/user/(center)/plist/page.tsx
+++ src/app/user/(center)/plist/page.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { Table } from "antd";
+import { Table, Divider } from "antd";
+
+// mock数据
+const dataSource = [
+  {
+    key: "1",
+    name: "zono精选",
+    age: 32,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "2",
+    name: "nope精选",
+    age: 42,
+    address: "西湖区湖底公园1号",
+  },
+  {
+    key: "3",
+    name: "nope精选2",
+    age: 41,
+    address: "西湖区湖底公园1号",
+  },
+];
+
+const columns = [
+  {
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    dataIndex: "age",
+    key: "age",
+  },
+  {
+    dataIndex: "address",
+    key: "address",
+  },
+];
 
 const UserProblemListPage = () => {
   return (
     <div>
-      <Table />
+      <Divider></Divider>
+      <Table showHeader={false} dataSource={dataSource} columns={columns} />;
     </div>
   );
 };
