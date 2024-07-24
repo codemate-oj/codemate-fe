d2c70425382cf43cd4f2a74ee2b72b1058e1313e
xiaokeqiang <xiaokeqiang@meituan.com>
2024-07-24 13:58:39 +0800

    feat: 完成创建表单的Modal

--- src/components/user/plist/create-quiz-model.tsx
+++ src/components/user/plist/create-quiz-model.tsx
@@ -1,24 +1,45 @@
 // import { useState } from "react";
-import { Input, Modal } from "antd";
+import { Modal, Form, Input, Button } from "antd";
+import { FC } from "react";
+
+interface CreateQuizModelProps {
+  ModalOpen: boolean;
+  onCancel: () => void;
+  onCreate: (values: unknown) => void;
+}
 
 /** @desc 创建题单modal */
-const CreateQuizModel = ({ ModalOpen }: { ModalOpen: boolean }) => {
-  // const [isModalOpen, setIsModalOpen] = useState(ModalOpen);
+const CreateQuizModel: FC<CreateQuizModelProps> = ({ ModalOpen, onCancel, onCreate }) => {
+  const [form] = Form.useForm();
 
-  const handleOk = () => {
-    // setIsModalOpen(false);
-  };
+  const handleOk = () => {};
 
   const handleCancel = () => {
-    // setIsModalOpen(false);
+    onCancel();
+  };
+
+  const handleCreate = () => {
+    form.validateFields().then((values) => {
+      onCreate(values);
+      form.resetFields();
+    });
   };
 
   return (
-    <Modal title="Basic Modal" open={ModalOpen} onOk={handleOk} onCancel={handleCancel}>
-      <p>题单名称</p>
-      <Input />
-      <p>题单描述</p>
-      <Input />
+    <Modal title="创建题单" open={ModalOpen} onOk={handleOk} onCancel={handleCancel} footer={null} centered>
+      <Form form={form} layout="vertical">
+        <Form.Item name="name" label="题单名称" rules={[{ required: true, message: "请输入题单名称" }]}>
+          <Input placeholder="A题单" />
+        </Form.Item>
+        <Form.Item name="description" label="题单描述" rules={[{ required: true, message: "请输入题单描述" }]}>
+          <Input.TextArea placeholder="C++暑假训练精选题目" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" onClick={handleCreate}>
+            确认创建
+          </Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 };
--- src/components/user/table-list.tsx
+++ src/components/user/table-list.tsx
@@ -8,7 +8,6 @@ import CreateQuizModel from "./plist/create-quiz-model";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const TableList = ({ children }: any) => {
   const pathname = usePathname();
-
   const [ModalOpen, setModalOpen] = useState(false);
   // TODO: 存入全局store中
   const [breadcrumbItems, setBreadcrumbItems] = useState([
@@ -16,9 +15,7 @@ const TableList = ({ children }: any) => {
       title: "我的中心",
     },
   ]);
-  const createQuiz = () => {
-    setModalOpen(true);
-  };
+
   useEffect((): void => {
     switch (userCenterRoutes.find((r) => pathname.startsWith(r.href))?.name) {
       case "我的题单":
@@ -38,20 +35,29 @@ const TableList = ({ children }: any) => {
           },
         ]);
     }
-  }, []);
+  }, [pathname]);
 
   return (
     <div className="flex-1">
       <h3 className="mb-5 flex items-center justify-between text-lg font-bold text-[#3D3D3D]">
         <Breadcrumb items={breadcrumbItems} />
         {userCenterRoutes.find((r) => pathname.startsWith(r.href))?.name === "我的题单" && (
-          <Button className="font-bold" onClick={createQuiz}>
+          <Button className="font-bold" onClick={() => setModalOpen(true)}>
             创建题单
           </Button>
         )}
       </h3>
       {children}
-      <CreateQuizModel ModalOpen={ModalOpen}></CreateQuizModel>
+      <CreateQuizModel
+        ModalOpen={ModalOpen}
+        onCancel={function (): void {
+          setModalOpen(false);
+        }}
+        onCreate={function (): void {
+          setModalOpen(false);
+          throw new Error("Function not implemented.");
+        }}
+      ></CreateQuizModel>
     </div>
   );
 };
