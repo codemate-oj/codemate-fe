26aeac3b2adfd9dd27b48804e51c666c7d71d9c3
xiaokeqiang <xiaokeqiang@meituan.com>
2024-07-24 21:30:59 +0800

    feat: 编写我的题单样式

--- src/app/user/(center)/plist/page.tsx
+++ src/app/user/(center)/plist/page.tsx
@@ -1,62 +1,73 @@
+"use client";
 import React from "react";
-import { Table, Divider } from "antd";
+import Image from "next/image";
+import { Table, Divider, TableProps } from "antd";
 
-// export interface ProblemList extends Document {
-//   docId: ObjectId;
-//   docType: typeof TYPE_PROBLEM_LIST;
-//   title: string;
-//   content: string;
-//   // 题单功能
-//   pids: number[];
-//   assign?: string[];
-//   visibility: "private" | "public" | "system"; // 控制题单可见性：该doc用于个人题单和系统题单
-//   // 树状题单功能
-//   parent: ObjectId | null;
-//   children?: ObjectId[];
-// }
+interface ProblemList {
+  key: string;
+  title: string;
+  content?: string;
+  pids: number[];
+  assign?: string[];
+  visibility?: "private" | "public" | "system";
+  children?: number[];
+  description?: string;
+}
 
-// mock数据
-const dataSource = [
+const dataSource: ProblemList[] = [
   {
     key: "1",
-    name: "zono精选",
-    age: 32,
-    address: "西湖区湖底公园1号",
+    title: "zono精选",
+    pids: [1, 2, 3],
+    description: "西湖区湖底公园1号",
   },
   {
     key: "2",
-    name: "nope精选",
-    age: 42,
-    address: "西湖区湖底公园1号",
+    title: "nope精选",
+    pids: [4, 5, 6, 7],
+    description: "西湖区湖底公园2号",
   },
   {
     key: "3",
-    name: "nope精选2",
-    age: 41,
-    address: "西湖区湖底公园1号",
+    title: "nope精选2",
+    pids: [2],
+    description: "西湖区湖底公园3号",
   },
 ];
 
-const columns = [
+const columns: TableProps<ProblemList>["columns"] = [
   {
-    dataIndex: "name",
-    key: "name",
+    title: "Title",
+    dataIndex: "title",
+    key: "title",
   },
   {
-    dataIndex: "age",
-    key: "age",
+    title: "pids",
+    key: "pids",
+    render: (_, { pids }) => <span className="text-sm text-[#FF7D37]">{`${pids.length} 道`}</span>,
   },
   {
-    dataIndex: "address",
-    key: "address",
+    title: "Description",
+    dataIndex: "description",
+    key: "description",
+  },
+
+  {
+    title: "Action",
+    key: "action",
+    render: () => (
+      <a>
+        <Image src="/svg/app-user-plist-editIcon.svg" alt="editQuiz" width={20} height={24} />
+      </a>
+    ),
   },
 ];
 
-const UserProblemListPage = () => {
+const UserProblemListPage: React.FC = () => {
   return (
     <div>
-      <Divider></Divider>
-      <Table showHeader={false} dataSource={dataSource} columns={columns} />;
+      <Divider />
+      <Table showHeader={false} dataSource={dataSource} columns={columns} />
     </div>
   );
 };
