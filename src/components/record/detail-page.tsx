e74a13b44032a2515e0045ab38c52ca5c790a6ae
WIN-QCFHI70G3KJ\EDY <xubaoqiang@macro-cloud.com>
2024-07-29 14:14:27 +0800

    # commit 添加评测结果页面（/record/:rid）用户代码高亮显示。

--- src/components/record/detail-page.tsx
+++ src/components/record/detail-page.tsx
@@ -6,6 +6,8 @@ import JudgeStatus from "./judge-status";
 import { parseTemplate } from "@/lib/utils";
 import TestCases from "./test-cases";
 import useRealtimeRecordDetail from "@/hooks/useRecordDetailConn";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dark } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export type RecordDoc = components["schemas"]["Record"];
 
@@ -16,6 +18,26 @@ interface IProps {
 
 const DetailPage: React.FC<IProps> = ({ defaultValue, rid }) => {
   const rdoc = useRealtimeRecordDetail(rid, defaultValue);
+  // TODO 模拟获取CODE
+  const showCode: boolean = true;
+  const codeType: string = "javascript";
+  const codeText: string = `
+    import React from 'react';
+    import Highlight, { Prism } from 'react-syntax-highlighter';
+    const codeString = 'function helloWorld() {
+        console.log('Hello, world!');
+    };'
+
+    // 测试高亮注释
+    function App() {
+        return (
+            <Highlight language="javascript" style={Prism}>
+                {codeString}
+            </Highlight>
+        );
+    }
+
+    export default App;`;
 
   if (!rdoc) {
     return null;
@@ -34,6 +56,23 @@ const DetailPage: React.FC<IProps> = ({ defaultValue, rid }) => {
         </div>
         {rdoc.testCases.length > 0 && <TestCases rid={rdoc._id} testCases={rdoc.testCases} />}
       </Card>
+
+      {/* TODO 这里需要后端一些判断规则，如只有编程题显示等等，整个上面可以添加一下骨架 */}
+      {showCode && (
+        <div>
+          <h3 className="text-lg font-bold">我的代码</h3>
+          <SyntaxHighlighter
+            showLineNumbers={true}
+            startingLineNumber={0}
+            language={codeType}
+            style={dark}
+            lineNumberStyle={{ color: "#ddd", fontSize: 20 }}
+            wrapLines={true}
+          >
+            {codeText.replace(/^\s+|\s+$/g, "")}
+          </SyntaxHighlighter>
+        </div>
+      )}
     </div>
   );
 };
